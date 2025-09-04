82e175104f45971c86c7ea5375eb73ba363c220c
adrien-J-B <jean.besegue@umail.uom.ac.mu>
2025-09-04 22:43:23 +0400

    Add files via upload

---
+++ blala/homepage.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Home", page_icon="🏠")
+
+st.sidebar.success("Select a page above.")
+
+st.title("Home Page")
+st.header( "Welcome to the Home Page! 🏠")
+
